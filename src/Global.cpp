dcda8c1d9238778edb433ad51d338a1890b4d5be
Carsten <magnum69@live.de>
2011-07-26 21:55:00 +0200

    moved code for class Global to Global.h and Global.cpp use non-static functions in Global, by introducing a global variable globalConfig

---
+++ src/Global.cpp
@@ -0,0 +1,134 @@
+
+#include <tbt/Global.h>
+
+using namespace std;
+
+
+namespace tbt
+{
+
+	Global globalConfig;
+
+
+	cl_device_type getType(const cl::Device &device)
+	{
+		cl_device_type deviceType;
+		device.getInfo(CL_DEVICE_TYPE, &deviceType);
+		return deviceType;
+	}
+
+
+	cl_uint getMaxComputeUnits(const cl::Device &device)
+	{
+		cl_uint nComputeUnits;
+		device.getInfo(CL_DEVICE_MAX_COMPUTE_UNITS, &nComputeUnits);
+		return nComputeUnits;
+	}
+
+
+	size_t getMaxWorkGroupSize(const cl::Device &device)
+	{
+		size_t maxWorkGroupSize;
+		device.getInfo(CL_DEVICE_MAX_WORK_GROUP_SIZE, &maxWorkGroupSize);
+		return maxWorkGroupSize;
+	}
+
+
+	cl_ulong getLocalMemSize(const cl::Device &device)
+	{
+		cl_ulong localMemSize;
+		device.getInfo(CL_DEVICE_LOCAL_MEM_SIZE, &localMemSize);
+		return localMemSize;
+	}
+
+
+	cl::Platform getStdPlatform(cl_device_type deviceType)
+	{
+		// Get a AMD/GPU and Intel/CPU platform and context.
+		cl::vector< cl::Platform > platformList;
+		cl::Platform::get(&platformList);
+
+		int indexAMD = -1, indexIntel = -1, indexNvidia = -1;
+		for(int i = 0; i < (int)platformList.size(); ++i) {
+			string platformName;
+			platformList[i].getInfo((cl_platform_info)CL_PLATFORM_NAME, &platformName);
+			if(platformName == "AMD Accelerated Parallel Processing")
+				indexAMD = i;
+			else if(platformName == "Intel(R) OpenCL")
+				indexIntel = i;
+			else if(platformName == "NVIDIA CUDA")
+				indexNvidia = i;
+		}
+
+		if(deviceType == CL_DEVICE_TYPE_GPU && indexAMD == -1 && indexNvidia == -1)
+			throw Error("No OpenCL platform for GPUs found!", Error::ecNoOpenCLPlatformFound);
+		
+		if(deviceType == CL_DEVICE_TYPE_CPU && indexAMD == -1 && indexIntel == -1)
+			throw Error("No OpenCL platform for CPUs found!", Error::ecNoOpenCLPlatformFound);
+
+		if(deviceType == CL_DEVICE_TYPE_GPU)
+			return platformList[(indexAMD != -1) ? indexAMD : indexNvidia];
+		else
+			return (indexIntel != -1) ? platformList[indexIntel] : platformList[indexAMD];
+	}
+
+
+	ostream &displayPlatformInfo(ostream &os)
+	{
+		cl::Platform platform = getPlatform();
+
+		string platformName;
+		platform.getInfo((cl_platform_info)CL_PLATFORM_NAME,    &platformName);
+		os << "    name:       " << platformName << endl;
+
+		string platformVendor;
+		platform.getInfo((cl_platform_info)CL_PLATFORM_VENDOR,  &platformVendor);
+		os << "    vendor:     " << platformVendor << endl;
+
+		string platformVersion;
+		platform.getInfo((cl_platform_info)CL_PLATFORM_VERSION, &platformVersion);
+		os << "    version:    " << platformVersion << endl;
+
+		string platformProfile;
+		platform.getInfo((cl_platform_info)CL_PLATFORM_PROFILE, &platformProfile);
+		os << "    profile:    " << platformProfile << endl;
+
+		string platformExtensions;
+		platform.getInfo((cl_platform_info)CL_PLATFORM_EXTENSIONS, &platformExtensions);
+		os << "    extensions: " << platformExtensions << endl;
+
+		return os;
+	}
+
+
+	void Global::createContext(cl_device_type deviceType, const cl::Platform &platform, cl_command_queue_properties properties)
+	{
+		m_platform = platform;
+
+		cl_context_properties cprops[3] = {
+			CL_CONTEXT_PLATFORM,
+			(cl_context_properties)(platform)(),
+			0
+		};
+
+		globalConfig.m_context = cl::Context(deviceType, cprops);
+		globalConfig.m_devCons.init(globalConfig.m_context, properties);
+
+		for(int i = m_devCons.numDevices()-1; i >= 0; i--) {
+			switch(m_devCons[i]->getType())
+			{
+			case CL_DEVICE_TYPE_CPU:
+				m_cpuDeviceIndex = i;
+			case CL_DEVICE_TYPE_GPU:
+				m_gpuDeviceIndex = i;
+			}
+		}
+	}
+
+
+	void createContext(cl_device_type deviceType, cl_command_queue_properties properties)
+	{
+		globalConfig.createContext(deviceType, getStdPlatform(deviceType), properties);
+	}
+
+}
---
+++ tbt/Global.h
@@ -0,0 +1,95 @@
+
+#ifndef _TBT_GLOBAL_H
+#define _TBT_GLOBAL_H
+
+#include <tbt/DeviceController.h>
+
+
+namespace tbt
+{
+
+	//! Maintenance of global platform and context, as well as some options.
+	class Global
+	{
+		cl::Platform m_platform;  //!< the global OpenCL platform we use.
+		cl::Context m_context;    //!< the global OpenCL context we use.
+
+		GlobalDeviceControllers m_devCons;  //!< controllers for all devices in the global context.
+		int m_cpuDeviceIndex;  //!< the index of the first CPU device in m_devCons (if any, otherwise -1).
+		int m_gpuDeviceIndex;  //!< the index of the first GPU device in m_devCons (if any, otherwise -1).
+
+		bool m_cacheProgramBinaries;           //!< shall we cache program (kernel) binaries at all?
+		bool m_recompileProgramsIfNewerDriver; //!< shall we check driver version and recompile programs if newer?
+
+	public:
+		//! Constructs a global configuration object.
+		Global() {
+			m_cpuDeviceIndex = m_gpuDeviceIndex = -1;
+			m_cacheProgramBinaries = true;
+			m_recompileProgramsIfNewerDriver = true;
+		}
+
+		//! Creates a context from a given platform and device type, and creates device controllers.
+		void createContext(cl_device_type deviceType, const cl::Platform &platform, cl_command_queue_properties properties = 0);
+
+		//! Returns current setting of option cacheProgramBinaries.
+		bool getCacheProgramBinaries() { return m_cacheProgramBinaries; }
+
+		//! Sets option cacheProgramBinaries to \a b.
+		void setCacheProgramBinaries(bool b) { m_cacheProgramBinaries = b; }
+
+		//! Returns current setting of option recompileProgramsIfNewerDriver.
+		bool getRecompileProgramsIfNewerDriver() { return m_recompileProgramsIfNewerDriver; }
+
+		//! Sets option recompileProgramsIfNewerDriver to \a b.
+		void setRecompileProgramsIfNewerDriver(bool b) { m_recompileProgramsIfNewerDriver = b; }
+
+		//! Returns global OpenCL platform.
+		cl::Platform getPlatform() { return m_platform; }
+
+		//! Returns global OpenCL context.
+		cl::Context getContext() { return m_context; }
+
+		//! Returns the first device controller (if any, otherwise 0 is returned).
+		DeviceController *getDeviceController() { return (m_devCons.numDevices() > 0) ? m_devCons[0] : 0; }
+
+		//! Returns a device controller for a CPU device (if any, otherwise 0 is returned).
+		DeviceController *getCPUDeviceController() { return (m_cpuDeviceIndex >= 0) ? m_devCons[m_cpuDeviceIndex] : 0; }
+
+		//! Returns a device controller for a GPU device (if any, otherwise 0 is returned).
+		DeviceController *getGPUDeviceController() { return (m_gpuDeviceIndex >= 0) ? m_devCons[m_gpuDeviceIndex] : 0; }
+	};
+
+	//! The (one and only) global configuration object.
+	extern Global globalConfig;
+
+
+	//! Create global context from given platform and device type.
+	inline void createContext(cl_device_type deviceType, const cl::Platform &platform, cl_command_queue_properties properties = 0) {
+		globalConfig.createContext(deviceType, platform, properties);
+	}
+
+	//! Create global context from given device type.
+	void createContext(cl_device_type deviceType, cl_command_queue_properties properties = 0);
+
+	//! Returns global OpenCL platform.
+	inline cl::Platform getPlatform() { return globalConfig.getPlatform(); }
+
+	//! Returns global OpenCL context.
+	inline cl::Context getContext() { return globalConfig.getContext(); }
+
+	//! Display information about global OpenCL platform.
+	std::ostream &displayPlatformInfo(std::ostream &os = std::cout);
+
+	//! Returns the first global device controller (if any, otherwise 0 is returned).
+	inline DeviceController *getDeviceController() { return globalConfig.getDeviceController(); }
+
+	//! Returns a global device controller for a CPU device (if any, otherwise 0 is returned).
+	inline DeviceController *getCPUDeviceController() { return globalConfig.getCPUDeviceController(); }
+
+	//! Returns a global device controller for a GPU device (if any, otherwise 0 is returned).
+	inline DeviceController *getGPUDeviceController() { return globalConfig.getGPUDeviceController(); }
+
+}
+
+#endif
