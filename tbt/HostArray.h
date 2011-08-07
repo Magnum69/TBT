443095dc0ad75815375b1d1e2d44ea436293f48d
Carsten <magnum69@live.de>
2011-08-07 21:33:23 +0200

    added iterators for host arrays

--- tbt/HostArray.h
+++ tbt/HostArray.h
@@ -8,6 +8,9 @@
 namespace tbt
 {
 
+	template<class T> class _HostArrayConstIterator;
+	template<class T> class _HostArrayIterator;
+
 	//! Array stored on the host.
 	/**
 	 * This class implements generic arrays with auomatic allocation and deallocation of memory,
@@ -23,6 +26,22 @@ namespace tbt
 		size_t m_nElements;  //!< number of elements in array.
 
 	public:
+		//! The type for iterator differences.
+		typedef index_t difference_type;
+
+		//! Const-iterator for device arrays.
+		typedef _HostArrayConstIterator<T> const_iterator;
+
+		//! Iterator for device arrays.
+		typedef _HostArrayIterator<T> iterator;
+
+		//! Const-reference type for mapped arrays.
+		typedef const T &const_reference;
+
+		//! Reference type for mapped arrays.
+		typedef T &reference;
+
+
 		//! Constructs an empty host array (for 0 elements).
 		HostArray() : m_ptr(0), m_nElements(0) { }
 
@@ -56,6 +75,57 @@ namespace tbt
 		size_t size() const {
 			return m_nElements;
 		}
+
+
+		/** @name Iterators
+		 * These methods return iterators to specific positions in the host array.
+		 */
+		//@{
+
+		//! Returns an iterator pointing to the first element in the array.
+		iterator begin();
+
+		//! Returns a const-iterator pointing to the first element in the array.
+		const_iterator begin() const;
+
+		//! Returns an iterator pointing to one past the last element in the array.
+		iterator end();
+
+		//! Returns a const-iterator pointing to one past the last element in the array.
+		const_iterator end() const;
+
+		//! Returns an iterator pointing to the first element in the reversed array.
+		/**
+		 * The returned iterator also points to the last element in the array.
+		 */
+		iterator rbegin();
+
+		//! Returns a const-iterator pointing to the first element in the reversed array.
+		/**
+		 * The returned iterator also points to the last element in the array.
+		 */
+		const_iterator rbegin() const;
+
+		//! Returns an iterator pointing to one past the last element in the reversed array.
+		/**
+		 * The returned iterator also points to one before the first element in the array.
+		 */
+		iterator rend();
+
+		//! Returns a const-iterator pointing to one past the last element in the reversed array.
+		/**
+		 * The returned iterator also points to one before the first element in the array.
+		 */
+		const_iterator rend() const;
+
+		//! Returns an iterator pointing to the <i>i</i>-th element in the array.
+		iterator at(index_t i);
+
+		//! Returns a const-iterator pointing to the <i>i</i>-th element in the array.
+		const_iterator at(index_t i) const;
+
+		//@}
+
 
 		//! Assignment operator. Copies the elements from b.
 		HostArray<T> &operator=(const HostArray<T> &b) {
@@ -107,6 +177,459 @@ namespace tbt
 				m_ptr[i] = b[i];
 		}
 	};
+
+
+	//! Const-iterator for host arrays.
+	template<class T>
+	class _HostArrayConstIterator
+	{
+		friend class HostArray<T>;
+
+		T *m_ptr;  //!< pointer to the element in the host array.
+
+		//! Constructs a const-iterator pointing to the element \a ptr points to.
+		/**
+		 * This constructor can only be called from class HostArray.
+		 */
+		_HostArrayConstIterator(const T *ptr) : m_ptr(ptr) { }
+
+	public:
+		//! Shorthand for this iterator type.
+		typedef _HostArrayConstIterator<T> _MyIter;
+
+		//! The type for iterator differences.
+		typedef typename HostArray<T>::difference_type difference_type;
+
+		//! The reference type for this iterator.
+		typedef typename HostArray<T>::const_reference reference;
+
+		/** @name Construction and Assignment
+		 * These constructors just provide a default constructor (creating an invalid iterator)
+		 * and copy constructors. To create an iterator pointing to a specific position in a
+		 * host array, use the methods provided by HostArray.
+		 */
+		//@{
+
+		//! Constructs an invalid const-iterator.
+		_HostArrayConstIterator() : m_ptr(0) { }
+
+		//! Copy constructor. Constructs a const-iterator pointing to the same position as \a iter.
+		_HostArrayConstIterator(const _MyIter &iter) : m_ptr(iter.m_ptr) { }
+
+		//! Constructs a const-iterator pointing to the same position as iterator \a iter.
+		_HostArrayConstIterator(const _HostArrayIterator<T> &iter) : m_ptr(iter.m_ptr) { }
+
+		//! Assignment operator.
+		_MyIter &operator=(const _MyIter &iter) {
+			m_ptr = iter.m_ptr;
+			return *this;
+		}
+
+		//@}
+
+
+		/** @name Access Methods
+		 * These methods provide access to the element this iterator points to.
+		 */
+		//@{
+
+		//! Returns a reference to the element in the host array this iterator points to.
+		reference operator*() const {
+			return *m_ptr;
+		}
+
+		//@}
+
+
+		/** @name Operators for Moving Iterators
+		 * These methods move an iterator forward or backward, or create a new iterator that points to a position
+		 * which is a specific number of elements before or after.
+		 */
+		//@{
+
+		//! Moves iterator one position forward (pre-increment).
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter &operator++() {
+			++m_ptr;
+			return *this;
+		}
+
+		//! Moves iterator one position forward (post-increment).
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter operator++(int) {
+			_MyIter iter(*this);
+			++m_ptr;
+			return iter;
+		}
+
+		//! Moves iterator one position backward (pre-increment).
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter &operator--() {
+			--m_ptr;
+			return *this;
+		}
+
+		//! Moves iterator one position backward (post-increment).
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter operator--(int) {
+			_DeviceArrayConstIterator<T> iter(*this);
+			--m_ptr;
+			return iter;
+		}
+
+		//! Moves iterator \a offset positions forward.
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter &operator+=(difference_type offset) {
+			m_ptr += offset;
+			return *this;
+		}
+
+		//! Returns an iterator pointing to a position \a offset positions forward of this iterator.
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter operator+(difference_type offset) const {
+			_DeviceArrayConstIterator<T> iter(*this);
+			return ( iter += offset);
+		}
+
+		//! Moves iterator \a offset positions backward.
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter &operator-=(difference_type offset) {
+			m_ptr -= offset;
+			return *this;
+		}
+
+		//! Returns an iterator pointing to a position \a offset positions backward of this iterator.
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter operator-(difference_type offset) const {
+			_DeviceArrayConstIterator<T> iter(*this);
+			return ( iter -= offset);
+		}
+
+		//! Returns the difference between two iterators.
+		/**
+		 * \pre This const-iterator and \a iter must both be valid and point to the same device array.
+		 *
+		 * @param iter must be a valid const-iterator pointing to the same device array as this const-iterator.
+		 * @return the difference between the two iterators, i.e., the number of elements in the
+		 *         interval [this iterator, \a iter); this difference will be 0 if both iterators
+		 *         point to the same position, and negative if \a iter points to a position before the
+		 *         position this iterator points to.
+		 */
+		difference_type operator-(const _MyIter &iter) const {
+			return m_ptr - iter.m_ptr;
+		}
+
+		//@}
+
+
+		/** @name Operators for Comparing Iterators
+		 * These operators compare iterators for equality / inequality, or for the relative position
+		 * of the elements they point to. The latter compare operators can only be called if the
+		 * two iterators to be compared are both valid and point to the same device array.
+		 */
+		//@{
+
+		//! Returns true if \a right points to the same position as this iterator; false otherwise.
+		bool operator==(const _MyIter &right) const {
+			return ( m_ptr == right.m_ptr );
+		}
+
+		//! Returns true if \a right points to a different position as this iterator; false otherwise.
+		bool operator!=(const _MyIter &right) const {
+			return ( m_ptr != right.m_ptr );
+		}
+
+		//! Returns true if this iterator points to a position before the position \a right points to.
+		/**
+		 * \pre \a both right and this iterator must be valid and point to the same array.
+		 */
+		bool operator<(const _MyIter &right) const {
+			return ( m_ptr < right.m_ptr );
+		}
+
+		//! Returns true if this iterator points to the same position or a position before the position \a right points to.
+		/**
+		 * \pre \a both right and this iterator must be valid and point to the same array.
+		 */
+		bool operator<=(const _MyIter &right) const {
+			return ( m_ptr <= right.m_ptr );
+		}
+
+		//! Returns true if this iterator points to a position after the position \a right points to.
+		/**
+		 * \pre \a both right and this iterator must be valid and point to the same array.
+		 */
+		bool operator>(const _MyIter &right) const {
+			return ( m_ptr > right.m_ptr );
+		}
+
+		//! Returns true if this iterator points to the same position or a position after the position \a right points to.
+		/**
+		 * \pre \a both right and this iterator must be valid and point to the same array.
+		 */
+		bool operator>=(const _MyIter &right) const {
+			return ( m_ptr >= right.m_ptr );
+		}
+
+		//@}
+	};
+
+
+	//! Const-iterator for host arrays.
+	template<class T>
+	class _HostArrayIterator
+	{
+		friend class HostArray<T>;
+		friend class _HostArrayConstIterator<T>;
+
+		T *m_ptr;  //!< pointer to the element in the host array.
+
+		//! Constructs a const-iterator pointing to the element \a ptr points to.
+		/**
+		 * This constructor can only be called from class HostArray.
+		 */
+		_HostArrayIterator(T *ptr) : m_ptr(ptr) { }
+
+	public:
+		//! Shorthand for this iterator type.
+		typedef _HostArrayIterator<T> _MyIter;
+
+		//! The type for iterator differences.
+		typedef typename HostArray<T>::difference_type difference_type;
+
+		//! The reference type for this iterator.
+		typedef typename HostArray<T>::reference reference;
+
+		/** @name Construction and Assignment
+		 * These constructors just provide a default constructor (creating an invalid iterator)
+		 * and copy constructors. To create an iterator pointing to a specific position in a
+		 * device array, use the methods provided by DeviceArray.
+		 */
+		//@{
+
+		//! Constructs an invalid const-iterator.
+		_HostArrayIterator() : m_ptr(0) { }
+
+		//! Copy constructor. Constructs a const-iterator pointing to the same position as \a iter.
+		_HostArrayIterator(const _MyIter &iter) : m_ptr(iter.m_ptr) { }
+
+		//! Assignment operator.
+		_MyIter &operator=(const _MyIter &iter) {
+			m_ptr = iter.m_ptr;
+			return *this;
+		}
+
+		//@}
+
+
+		/** @name Access Methods
+		 * These methods provide access to the element this iterator points to.
+		 */
+		//@{
+
+		//! Returns a reference to the element in the host array this iterator points to.
+		reference operator*() const {
+			return *m_ptr;
+		}
+
+		//@}
+
+
+		/** @name Operators for Moving Iterators
+		 * These methods move an iterator forward or backward, or create a new iterator that points to a position
+		 * which is a specific number of elements before or after.
+		 */
+		//@{
+
+		//! Moves iterator one position forward (pre-increment).
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter &operator++() {
+			++m_ptr;
+			return *this;
+		}
+
+		//! Moves iterator one position forward (post-increment).
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter operator++(int) {
+			_MyIter iter(*this);
+			++m_ptr;
+			return iter;
+		}
+
+		//! Moves iterator one position backward (pre-increment).
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter &operator--() {
+			--m_ptr;
+			return *this;
+		}
+
+		//! Moves iterator one position backward (post-increment).
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter operator--(int) {
+			_DeviceArrayConstIterator<T> iter(*this);
+			--m_ptr;
+			return iter;
+		}
+
+		//! Moves iterator \a offset positions forward.
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter &operator+=(difference_type offset) {
+			m_ptr += offset;
+			return *this;
+		}
+
+		//! Returns an iterator pointing to a position \a offset positions forward of this iterator.
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter operator+(difference_type offset) const {
+			_DeviceArrayConstIterator<T> iter(*this);
+			return ( iter += offset);
+		}
+
+		//! Moves iterator \a offset positions backward.
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter &operator-=(difference_type offset) {
+			m_ptr -= offset;
+			return *this;
+		}
+
+		//! Returns an iterator pointing to a position \a offset positions backward of this iterator.
+		/**
+		 * \pre This const-iterator must be valid.
+		 */
+		_MyIter operator-(difference_type offset) const {
+			_DeviceArrayConstIterator<T> iter(*this);
+			return ( iter -= offset);
+		}
+
+		//! Returns the difference between two iterators.
+		/**
+		 * \pre This const-iterator and \a iter must both be valid and point to the same device array.
+		 *
+		 * @param iter must be a valid const-iterator pointing to the same device array as this const-iterator.
+		 * @return the difference between the two iterators, i.e., the number of elements in the
+		 *         interval [this iterator, \a iter); this difference will be 0 if both iterators
+		 *         point to the same position, and negative if \a iter points to a position before the
+		 *         position this iterator points to.
+		 */
+		difference_type operator-(const _MyIter &iter) const {
+			return m_ptr - iter.m_ptr;
+		}
+
+		//@}
+
+
+		/** @name Operators for Comparing Iterators
+		 * These operators compare iterators for equality / inequality, or for the relative position
+		 * of the elements they point to. The latter compare operators can only be called if the
+		 * two iterators to be compared are both valid and point to the same device array.
+		 */
+		//@{
+
+		//! Returns true if \a right points to the same position as this iterator; false otherwise.
+		bool operator==(const _MyIter &right) const {
+			return ( m_ptr == right.m_ptr );
+		}
+
+		//! Returns true if \a right points to a different position as this iterator; false otherwise.
+		bool operator!=(const _MyIter &right) const {
+			return ( m_ptr != right.m_ptr );
+		}
+
+		//! Returns true if this iterator points to a position before the position \a right points to.
+		/**
+		 * \pre \a both right and this iterator must be valid and point to the same array.
+		 */
+		bool operator<(const _MyIter &right) const {
+			return ( m_ptr < right.m_ptr );
+		}
+
+		//! Returns true if this iterator points to the same position or a position before the position \a right points to.
+		/**
+		 * \pre \a both right and this iterator must be valid and point to the same array.
+		 */
+		bool operator<=(const _MyIter &right) const {
+			return ( m_ptr <= right.m_ptr );
+		}
+
+		//! Returns true if this iterator points to a position after the position \a right points to.
+		/**
+		 * \pre \a both right and this iterator must be valid and point to the same array.
+		 */
+		bool operator>(const _MyIter &right) const {
+			return ( m_ptr > right.m_ptr );
+		}
+
+		//! Returns true if this iterator points to the same position or a position after the position \a right points to.
+		/**
+		 * \pre \a both right and this iterator must be valid and point to the same array.
+		 */
+		bool operator>=(const _MyIter &right) const {
+			return ( m_ptr >= right.m_ptr );
+		}
+
+		//@}
+	};
+
+
+	template<class T> inline
+	typename HostArray<T>::iterator HostArray<T>::begin() { return iterator(m_ptr); }
+
+	template<class T> inline
+	typename HostArray<T>::const_iterator HostArray<T>::begin() const { return const_iterator(m_ptr); }
+
+	template<class T> inline
+	typename HostArray<T>::iterator HostArray<T>::end() { return iterator(m_ptr+m_nElements); }
+
+	template<class T> inline
+	typename HostArray<T>::const_iterator HostArray<T>::end() const { return const_iterator(m_ptr+m_nElements); }
+
+	template<class T> inline
+	typename HostArray<T>::iterator HostArray<T>::rbegin() { return iterator(m_ptr+m_nElements-1); }
+
+	template<class T> inline
+	typename HostArray<T>::const_iterator HostArray<T>::rbegin() const { return const_iterator(m_ptr+m_nElements-1); }
+
+	template<class T> inline
+	typename HostArray<T>::iterator HostArray<T>::rend() { return iterator(m_ptr-1); }
+
+	template<class T> inline
+	typename HostArray<T>::const_iterator HostArray<T>::rend() const { return const_iterator(m_ptr-1); }
+
+	template<class T> inline
+	typename HostArray<T>::iterator HostArray<T>::at(index_t i) { return iterator(m_ptr+i); }
+
+	template<class T> inline
+	typename HostArray<T>::const_iterator HostArray<T>::at(index_t i) const { return const_iterator(m_ptr+i); }
 
 }
 
